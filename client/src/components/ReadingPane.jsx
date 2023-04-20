c536cd62736cdbc91cfb5d577e4cf9f650d72d5a
Brooke <bmhesseltine@ksu.edu>
2023-04-19 19:18:22 -0500

    Added Component when user clicks link

--- client/src/components/ReadingPane.jsx
+++ client/src/components/ReadingPane.jsx
@@ -20,7 +20,11 @@ const ReadingPane = React.memo( props => {
       setHtml(html);
       setLoading(false);
     })
+
+
   })
+
+
 
   if(loading) return <>Loading...</>
 
@@ -29,5 +33,38 @@ const ReadingPane = React.memo( props => {
   )
 
 })
+
+const Component = () => {
+    useEffect(() => {
+        window.addEventListener('beforeunload', alertUser)
+        window.addEventListener('unload', handleEndConcert)
+        return () => {
+            window.removeEventListener('beforeunload', alertUser)
+            window.removeEventListener('unload', handleEndConcert)
+            handleEndConcert()
+        }
+    }, [])
+
+    const alertUser = e => {
+        e.preventDefault()
+        e.returnValue = ''
+    }
+
+    const handleEndConcert = async () => {
+        await fetcher({
+            url: endConcert(concert.id),
+            method: 'PUT'
+        })
+    }
+
+    return (
+        <Container>
+            <Prompt
+                when={isPrompt()}
+                message={() => 'Are you sure you want to leave this page?'}
+            />
+        </Container>
+    )
+}
 
 export default ReadingPane;

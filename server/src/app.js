9937587f1ee427f479cee10c8fea6fed6efe3f3d
bmhesseltine <69606508+bmhesseltine@users.noreply.github.com>
2023-05-02 12:48:44 -0500

    Added Documentation

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use( bodyParser.json() )
 
+//API call that gets the links of textbook pages to show in the navigation panel
 app.get('/api/assigned-readings', (req, res) => {
   res.json({
       0: { id: 0, title: "CIS 400 Textbook", url: "https://textbooks.cs.ksu.edu/cis400/" },
@@ -14,6 +15,7 @@ app.get('/api/assigned-readings', (req, res) => {
   })
 })
 
+//API call that embeds hypothesis into html
 app.post('/api/reading', async (req, res) => {
   const response = await axios.get(req.body.url);
   res.send(response.data.replace('</body>', '<script src="https://hypothes.is/embed.js" async></script></body>'))
